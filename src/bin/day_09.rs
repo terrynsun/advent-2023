928a2126c75790251441d4d84853a081c1c8a27f
Terry Sun <terry@onesignal.com>
2023-12-09 00:24:52 -0800

    Day 9: finally skipping some so I can collect more leaderboard points

---
+++ src/bin/day_09.rs
@@ -0,0 +1,57 @@
+use advent_2023::puzzle::Puzzle;
+use advent_2023::util::split_to_i64s;
+
+fn next(vs: &Vec<i64>) -> Vec<i64> {
+    vs.windows(2).map(|v| v[1] - v[0]).collect::<Vec<i64>>()
+}
+
+fn zeros(vs: &Vec<i64>) -> bool {
+    vs.iter().all(|v| *v == 0)
+}
+
+fn gen_seq(vs: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut cur = vs.clone();
+    let mut acc = vec![];
+
+    while !zeros(&cur) {
+        acc.push(cur.clone());
+        cur = next(&cur);
+    }
+
+    acc.push(cur.clone());
+
+    acc
+}
+
+// -- part 1 -- //
+fn predict_next(vs: &Vec<i64>) -> i64 {
+    gen_seq(vs).iter().map(|seq| seq[seq.len()-1]).sum::<i64>()
+}
+
+fn a(data: &Vec<Vec<i64>>) -> i64 {
+    data.iter().map(predict_next).sum()
+}
+
+// -- part 2 -- //
+fn predict_prev(vs: &Vec<i64>) -> i64 {
+    gen_seq(vs).iter()
+        .map(|seq| seq[0])
+        .rev()
+        .fold(0, |acc, x| x-acc)
+}
+
+fn b(data: &Vec<Vec<i64>>) -> i64 {
+    data.iter().map(predict_prev).sum()
+}
+
+fn main() {
+    Puzzle {
+        name: "9",
+        parts: vec![a, b],
+        delimiter: '\n',
+        preprocess: |text| {
+            text.iter()
+                .map(|line| split_to_i64s(line)).collect()
+        },
+    }.solve();
+}
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,20 @@
+pub fn split_to_strings(s: &str) -> Vec<String> {
+    s.trim_end()
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect()
+}
+
 pub fn split_to_u64s(s: &str) -> Vec<u64> {
     s.trim_end()
         .split_whitespace()
-        .map(|s| s.parse::<u64>().unwrap())
+        .map(|s| s.parse::<_>().unwrap())
+        .collect()
+}
+
+pub fn split_to_i64s(s: &str) -> Vec<i64> {
+    s.trim_end()
+        .split_whitespace()
+        .map(|s| s.parse::<_>().unwrap())
         .collect()
 }
